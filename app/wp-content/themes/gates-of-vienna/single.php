3e65a26c87b4afc9d2273668d7b30941252035d2
Luke Shumard <lukeshumard@gmail.com>
2014-12-10 14:07:59 +0000

    terrible post navigation

--- app/wp-content/themes/gates-of-vienna/single.php
+++ app/wp-content/themes/gates-of-vienna/single.php
@@ -1,5 +1,11 @@
 <section id="content">
   
+  <div class="container">
+    <p>
+      <a href="/">Main site</a> / <a href="/blog/">Blog</a>
+    </p>
+  </div>
+
   <article class="article container clearfix">
     
     <h1 class="title"><? the_title(); ?></h1>
